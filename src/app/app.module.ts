18fb8ecd8a62b5295cf6c0813ba591510ed9197c
Daniel Carneiro <geral4x@gmail.com>
2018-07-28 00:10:01 +0100

    initial firebase setup
    Signed-off-by: Daniel Carneiro <geral4x@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCASPlZlMzNedR0w2m913OpCQmMlWFWElI",
+  authDomain: "amplified-vine-112019.firebaseapp.com",
+  databaseURL: "https://amplified-vine-112019.firebaseio.com",
+  projectId: "amplified-vine-112019",
+  storageBucket: "amplified-vine-112019.appspot.com",
+  messagingSenderId: "533268361339"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
